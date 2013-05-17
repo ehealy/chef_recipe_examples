47669ce0365e6fc8a642efedfb9315cf63e7479e
ehealy <ehealy@gmail.com>
2013-05-17 11:00:31 -0300

    Update default.rb
    generalized application database name.

--- postgresql_server_standby/attributes/default.rb
+++ postgresql_server_standby/attributes/default.rb
@@ -191,7 +191,7 @@ default['postgresql']['pg_hba'] = [
   {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'peer'},
   {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
   {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'},
-  {:type => 'hostssl', :db => 'myenergy', :user => 'myenergy', :addr => '0.0.0.0 0.0.0.0', :method => 'md5'}
+  {:type => 'hostssl', :db => 'application_db', :user => 'application_db', :addr => '0.0.0.0 0.0.0.0', :method => 'md5'}
 ]
 
 default['postgresql']['password'] = Hash.new
