51926f71c86c9b5202bf0f19e5fb84d0818f8aca
ehealy <ehealy@gmail.com>
2013-05-17 10:55:30 -0300

    Update start_collector.sh
    generalized application name.

--- rails_program/files/default/start_collector.sh
+++ rails_program/files/default/start_collector.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Scipt used by monit to start collector processes
+# Scipt used by monit to start application processes
 
 . /etc/profile.d/rbenv.sh
 
 export PATH=$PATH:/usr/local/bin
 
-cd /home/ubuntu/collector/current
-VERBOSE=0 RAILS_ENV=production QUEUE=status,priority,calculate,extract,retrieve,store,scrape rake resque:work >/home/ubuntu/collector_$1.log 2>&1 &
+cd /home/ubuntu/application/current
+VERBOSE=0 RAILS_ENV=production QUEUE=status,priority,calculate,extract,retrieve,store,scrape rake resque:work >/home/ubuntu/application_process_$1.log 2>&1 &
 echo $! > /home/ubuntu/resque_$1.pid
